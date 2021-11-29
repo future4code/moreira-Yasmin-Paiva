614567118eea47ef284bd88e652fb8af6f413d41
Yasmin-Paiva <yasmingpaivas@gmail.com>
2021-11-29 18:47:45 -0300

    arquivo-html-e-js-adicionados

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,43 @@
+/* Exercicio 1:
+10
+10 5
+
+Exercício 2:
+10 10 10
+
+Exercício 3:
+let horasDeTrabalho = prompt("Quantas horas você trabalha por dia?")
+let salario = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${t/p} por hora`)
+
+*/
+
+/* let nome
+typeof nome
+
+let idade
+typeof idade
+
+console.log(nome, idade) */
+
+/* Ao fazer a variavel sem um valor usando const aparece o seguinte
+erro: Uncaught SyntaxError: Missing initializer in const declaration. */
+
+/* Após fazer com let consegui com que aparecesse "undefined", o que quer
+dizer que nenhum valor foi atribuído a variavel. */
+
+
+/* const nome = prompt("Olá, qual seu nome?")
+const idade = prompt("Qual a sua idade?")
+console.log(nome, idade) */
+
+/* Erro na linha 30 pois o codigo anterior estava atrapalhando, logo
+o coloquei como comentario e resolveu. Foi impresso "Yasmin 19" */
+
+const nome = prompt("Olá, qual seu nome?")
+const idade = prompt("Qual a sua idade?")
+console.log("Olá", nome, ", você tem", idade, "anos.")
+
+
+
+
